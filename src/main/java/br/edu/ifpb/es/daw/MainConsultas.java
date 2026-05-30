f2769b4c0fa6370f7a8d639d4f4f99e1b2e8c26a
José Arthur <ja1364091@gmail.com>
2026-05-30 19:36:40 -0300

    feat: criação do esqueleto da classe de consultas

---
+++ src/main/java/br/edu/ifpb/es/daw/MainConsultas.java
@@ -0,0 +1,19 @@
+package br.edu.ifpb.es.daw;
+
+public class MainConsultas {
+    public static void main(String[] args) {
+        System.out.println("======= INICIANDO TESTE DE CONSULTAS =======");
+
+        // [2.1] Teste da Consulta de Estoque Crítico
+
+        // [2.3] Teste da Consulta de Faixa de Preço
+
+        // [2.4] Teste da Consulta de Média de Notas na Avaliação
+
+        // [2.2] Teste da Consulta de Pedidos por Usuário
+
+        // [2.5] Teste da Consulta de JOIN FETCH (Pedido com itens)
+
+        System.out.println("======= FIM DOS TESTES =======");
+    }
+}
